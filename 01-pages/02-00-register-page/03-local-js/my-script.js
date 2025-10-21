fe33914cb3ab9f41fb61d57afea32249f0995f1c
Non <non.sin@dome.tu.ac.th>
2025-10-21 18:28:33 +0700

    Update validation register page phone number

--- 01-pages/02-00-register-page/03-local-js/my-script.js
+++ 01-pages/02-00-register-page/03-local-js/my-script.js
@@ -1,17 +1,35 @@
-      const input = document.querySelector(".email"),
+      const input1 = document.querySelector(".email"),
               emailIcon = document.querySelector(".email-icon")
 
-        input.addEventListener("keyup", () =>{
+        input1.addEventListener("keyup", () =>{
             let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
-            if(input.value === ""){
+            if(input1.value === ""){
                 return console.log("input is empty")
             }
-            if(input.value.match(pattern)){
+            if(input1.value.match(pattern)){
                 emailIcon.classList.replace("fa-envelope", "fa-circle-check");
                 return emailIcon.style.color = "#4bb543"
             }
             emailIcon.classList.replace("fa-circle-check", "fa-envelope");
             return emailIcon.style.color = "red"
         }
-    
+    )
+
+        const input = document.querySelector(".tel"),
+        telIcon = document.querySelector(".tel-icon")
+
+        input.addEventListener("keyup", () =>{
+            let pattern = /[0-9]{9}/;
+            if(input.value === ""){
+                return console.log("input is empty")
+            }
+            if(input.value.match(pattern)){
+                telIcon.classList.replace("fa-phone", "fa-circle-check");
+                return telIcon.style.color = "#4bb543"
+            }
+            telIcon.classList.replace("fa-circle-check", "fa-phone");
+            return telIcon.style.color = "red"
+        }
     );
+
+    
