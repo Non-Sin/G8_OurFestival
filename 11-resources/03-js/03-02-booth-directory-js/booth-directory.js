97d462edf427df45eef2107e1196b20853c46ccd
Non <non.sin@dome.tu.ac.th>
2025-11-23 00:36:27 +0700

    Update Path to booth directory page

---
+++ 11-resources/03-js/03-02-booth-directory-js/booth-directory.js
@@ -0,0 +1,70 @@
+ var myIndex1 = 0;
+    var myIndex2 = 0;
+    var myIndex3 = 0;
+    var myIndex4 = 0;
+
+    carousel1();
+    carousel2();
+    carousel3();
+    carousel4();
+
+    // For slides1
+    function carousel1() {
+        var i;
+        var x = document.getElementsByClassName("mySlides1");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        myIndex1++;
+        if (myIndex1 > x.length) { myIndex1 = 1 }
+        x[myIndex1 - 1].style.display = "block";
+        setTimeout(carousel1, 3000);
+    }
+
+    // For slides2
+    function carousel2() {
+        var i;
+        var s = document.getElementsByClassName("mySlides2");
+        for (i = 0; i < s.length; i++) {
+            s[i].style.display = "none";
+        }
+        myIndex2++;
+        if (myIndex2 > s.length) { myIndex2 = 1 }
+        s[myIndex2 - 1].style.display = "block";
+        setTimeout(carousel2, 3000);
+    }
+
+    // For slides3
+    function carousel3() {
+        var i;
+        var y = document.getElementsByClassName("mySlides3");
+        for (i = 0; i < y.length; i++) {
+            y[i].style.display = "none";
+        }
+        myIndex3++;
+        if (myIndex3 > y.length) { myIndex3 = 1 }
+        y[myIndex3 - 1].style.display = "block";
+        setTimeout(carousel3, 3000);
+    }
+
+    // For slides4
+    function carousel4() {
+        var i;
+        var z = document.getElementsByClassName("mySlides4");
+        for (i = 0; i < z.length; i++) {
+            z[i].style.display = "none";
+        }
+        myIndex4++;
+        if (myIndex4 > z.length) { myIndex4 = 1 }
+        z[myIndex4 - 1].style.display = "block";
+        setTimeout(carousel4, 3000);
+    }
+
+    const btn = document.querySelector(".mobile-menu-btn");
+    const menu = document.querySelector(".mobile-menu");
+    const arrow = document.querySelector(".mobile-menu-btn .arrow");
+
+    btn.addEventListener("click", () => {
+        menu.style.display = menu.style.display === "block" ? "none" : "block";
+        arrow.classList.toggle("rotate");
+    });
