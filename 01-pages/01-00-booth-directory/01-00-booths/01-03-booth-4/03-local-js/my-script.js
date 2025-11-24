a9b154c8ccbdda165e9c0ab5c8cd26d2fd174a0c
khemjira saenkla6809617001 <Khemjira.sae@dome.tu.ac.th>
2025-11-24 21:20:14 +0700

    fix path footer and header

--- 01-pages/01-00-booth-directory/01-00-booths/01-03-booth-4/03-local-js/my-script.js
+++ 01-pages/01-00-booth-directory/01-00-booths/01-03-booth-4/03-local-js/my-script.js
@@ -58,4 +58,7 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
- 
+const toggle = document.querySelector("header .menu-toggle");
+const menu = document.querySelector("header .nav-menu");
+
+        toggle.addEventListener("click", () => menu.classList.toggle("active"));
